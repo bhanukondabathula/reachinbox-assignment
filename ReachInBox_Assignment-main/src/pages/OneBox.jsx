d79143d911639b3bf55c6d1df63f3196f3c9e2ff
bhanu <152006738+bhanukondabathula@users.noreply.github.com>
2024-08-25 19:05:12 +0530

    OneBox.jsx

--- ReachInBox_Assignment-main/src/pages/OneBox.jsx
+++ ReachInBox_Assignment-main/src/pages/OneBox.jsx
@@ -1,29 +1,58 @@
-import { useEffect, useState } from 'react';
-import SideBar from '../components/SideBar';
-import MainPage from '../components/MainPage';
-import RightSection from '../components/RightSection';
-import AppBar from '../components/AppBar';
+// OneBox.js
+import { useEffect, useState } from "react";
+import SubView from "../components/SubView";
+import MainPage from "../components/MainPage";
+import SideBar from "../components/SideBar";
+import TopBar from "../components/TopBar";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const OneBox = () => {
-  const [currentTheme, setCurrentTheme] = useState('light');
+function OneBox() {
+  const Navigate = useNavigate();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const token = queryParams.get("token");
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-      setCurrentTheme(savedTheme);
+    if (!token) {
+      Navigate("/login");
     }
-  }, []);
+    if (token) {
+      localStorage.setItem("token", `Bearer ${token}`);
+    }
+  }, [token]);
+
+  const [selectedComponent, setSelectedComponent] = useState(null); 
+
+  const handleMenuItemClick = (path: any) => {
+    setSelectedComponent(path);
+  };
+
+  if (selectedComponent === null) {
+    return (
+      <div className="h-screen w-screen dark:bg-black bg-white pl-14">
+        <SideBar onMenuItemClick={handleMenuItemClick} />
+        <TopBar />
+        <SubView />
+      </div>
+    );
+  }
 
   return (
-    <div className={`flex flex-col h-screen ${currentTheme}`}>
-      <AppBar />
-      <div className="flex-grow flex overflow-hidden">
-        <SideBar />
-        <MainPage />
-        <RightSection />
+    <div className="h-screen w-screen dark:bg-black bg-white pl-14">
+      <SideBar onMenuItemClick={handleMenuItemClick} />
+      <TopBar />
+      <div>
+        {/* Render the selected component */}
+        {selectedComponent === "/" && <SubView />}
+        {selectedComponent === "/search" && <SubView />}
+        {selectedComponent === "/mail" && <SubView />}
+        {selectedComponent === "/send" && <SubView />}
+        {selectedComponent === "/stack" && <SubView />}
+        {selectedComponent === "/inbox" && <MainPage />}
+        {selectedComponent === "/stacks" && <SubView />}
       </div>
     </div>
   );
-};
+}
 
 export default OneBox;
